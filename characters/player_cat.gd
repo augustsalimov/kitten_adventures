5bedc9e935631c913c5fbf4174d4e95c34a28127
Guseyn Salimov <maximweller@protonmail.com>
2024-01-17 00:46:47 +0100

    character moves & animation

--- characters/player_cat.gd
+++ characters/player_cat.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0, 1)
+
+@onready var animation_tree = $AnimationTree
+
+func _ready():
+	update_animation_params(starting_direction)
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	print(input_direction)
+	
+	update_animation_params(input_direction)
 	
 	velocity = input_direction * move_speed
 	move_and_slide()
+	
+func update_animation_params(move_input: Vector2):
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)
